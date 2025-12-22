ff695fa05d874199e9fd08e3278271619dedf1b9
Mathews Mittathani <mathewsmittathani@gmail.com>
2025-12-22 14:02:32 +0530

    done

--- new_PHPMailer/admins_email.php
+++ new_PHPMailer/admins_email.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 //email to  admins(ip) when ticket is OPEN                                              
 //email to  admins(op) when ticket is OPEN                                              
 //email to  admins(interim) when ticket is OPEN                                         
